a03a049dc2d4b1da7034347ba5d0744a8a8d7161
chek taty <orchidetaty@gmail.com>
2021-01-05 14:52:36 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
     <Navbar />
-    <Table />
+    <Graph />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import Table from "./components/Table.vue";
+import Graph from "./components/Graph.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
-    Table,
+    Graph,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './custom.scss'
+import VueApexCharts from "vue-apexcharts";
 
+
+
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
