772f6e42366e2581eefcff8480b530d3383e188f
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2021-01-06 14:27:35 +0100

    edit publicPath name for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,49 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Table from '../components/Table.vue'
-import Map from '../components/Map.vue'
-import Graph from '../components/Graph.vue'
-import TableFiltre from '../components/TableFiltre.vue'
-import About from '../components/About.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Table from "../components/Table.vue";
+import Map from "../components/Map.vue";
+import Graph from "../components/Graph.vue";
+import TableFiltre from "../components/TableFiltre.vue";
+import About from "../components/About.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Table',
-    component: Table
+    path: "/",
+    name: "Table",
+    component: Table,
   },
   {
-    path: '/table',
-    name: 'Table',
-    component: Table
+    path: "/table",
+    name: "Table",
+    component: Table,
   },
   {
-    path: '/map',
-    name: 'Map',
-    component: Map
+    path: "/map",
+    name: "Map",
+    component: Map,
   },
   {
-    path: '/graph',
-    name: 'Graph',
-    component: Graph
-  },
- {
-    path: '/TableFiltre',
-    name: 'TableFiltre',
-    component: TableFiltre
+    path: "/graph",
+    name: "Graph",
+    component: Graph,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/TableFiltre",
+    name: "TableFiltre",
+    component: TableFiltre,
   },
-]
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
 
 const router = new VueRouter({
-   mode:"history",
-  routes
-})
+  mode: "hash",
+  routes,
+});
 
-export default router
+export default router;
